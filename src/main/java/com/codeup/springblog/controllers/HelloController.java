e4a384fdce155b619a2b964fa3f0c098615fdafc
Savanna Marie <savannamarie@gmail.com>
2021-07-20 18:24:34 -0500

    Spring intro

---
+++ src/main/java/com/codeup/springblog/controllers/HelloController.java
@@ -0,0 +1,34 @@
+package com.codeup.springblog.controllers;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+@Controller
+public class HelloController {
+    @GetMapping("/hello")
+    @ResponseBody
+    public String hello() {
+        return "<h1>Hello from Spring!</h1>";
+    }
+
+    @GetMapping("/hello/{name}")
+    public String sayHello(@PathVariable String name, Model model){
+        model.addAttribute("name", name);
+        return "hello";
+    }
+    @GetMapping("/join")
+    public String showJoinForm(){
+        return "join";
+    }
+    @PostMapping("/join")
+    public String joinCohort(@RequestParam(name = "cohort") String cohort, Model model) {
+        model.addAttribute("cohort", "Welcome to " + cohort + "!");
+        return "join";
+    }
+
+    @GetMapping("/hello/in/{color}")
+    @ResponseBody
+    public String helloInColor (@PathVariable String color) {
+        return "<h1 style=\"color: " + color + "\">Hello in " + color + "!</h1>";
+    }
+}
