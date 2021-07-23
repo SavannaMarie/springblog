680d1619ea572c5b4d807a62ed233cf59d95bd3f
Savanna Marie <savannamarie@gmail.com>
2021-07-23 13:45:29 -0500

    Doing some major refactoring

--- src/main/java/com/codeup/springblog/controllers/PostController.java
+++ src/main/java/com/codeup/springblog/controllers/PostController.java
@@ -1,5 +1,6 @@
 package com.codeup.springblog.controllers;
 import com.codeup.springblog.models.Post;
+import com.codeup.springblog.models.PostRepository;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
@@ -9,10 +10,15 @@ import java.util.List;
 
 @Controller
 public class PostController {
-    private List<Post> posts = new ArrayList<>();
+    private final PostRepository postDao;
+    public PostController(PostRepository postDao) {
+        this.postDao = postDao;
+    }
+
 
     @GetMapping("/posts")
     public String viewPosts(Model model) {
+        List<Post> posts = new ArrayList<>();
         posts.add(new Post("This is post1", "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."));
         posts.add(new Post("This is post2", "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."));
         model.addAttribute("posts", posts);
@@ -27,9 +33,9 @@ public class PostController {
     }
 
     @GetMapping("/posts/create")
-    @ResponseBody
-    public String createForm() {
-        return "View form to create a post.";
+    public String viewCreateForm(Model viewModel) {
+        viewModel.addAttribute("post", new Post());
+        return "posts/create";
     }
 
     @PostMapping("/posts/create")
--- src/main/java/com/codeup/springblog/models/Post.java
+++ src/main/java/com/codeup/springblog/models/Post.java
@@ -1,14 +1,37 @@
 package com.codeup.springblog.models;
+import javax.persistence.*;
+
+@Entity
+@Table(name = "posts")
 public class Post {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Column(nullable = false)
     private String title;
+
+    @Column(nullable = false, columnDefinition = "TEXT")
     private String body;
 
+    public Post() {}
+
+    // CREATE
     public Post(String title, String body) {
         this.title = title;
         this.body = body;
     }
 
-    public Post(){}
+    //READ
+    public Post(Long id, String title, String body) {
+        this.id = id;
+        this.title = title;
+        this.body = body;
+    }
+
+    public Long getId() { return id; }
+
+    public void setId(Long id) { this.id = id; }
 
     public String getTitle() {
         return title;
@@ -25,4 +48,5 @@ public class Post {
     public void setBody(String body) {
         this.body = body;
     }
+
 }
